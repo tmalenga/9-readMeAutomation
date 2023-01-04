83c79451f697c176e8d557c24be146d879708d7e
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-01-04 19:45:22 -0400

    Add initial files and test prompt

---
+++ index.js
@@ -0,0 +1,33 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { response } = require('express');
+
+// TODO: Create an array of questions for user input
+inquirer
+.prompt(
+// const questions = 
+[
+    {
+        type: "input",
+        message: "What is the title of the project:  ",
+        name: "title",
+    },
+    
+    {
+        type: "input",
+        message: "Project description:  ",
+        name: "description",
+    }
+]);
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
