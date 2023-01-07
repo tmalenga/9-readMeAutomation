550148ae570c1c9229210061ae3fffacdc5dd5ed
tmalenga0@gmail.com <tmalenga0@gmail.com>
2023-01-07 15:58:51 -0400

    Update working code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { response } = require('express');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 // inquirer
@@ -52,9 +53,16 @@ const questions =
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data)
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data)
+// }
+
+function writeToFile(userInput) {
+    fs.writeFile('./generatedreadMe.md', userInput, function(err){
+        if (err) throw err
+        console.log("success");
+    })
 }
 
 // TODO: Create a function to initialize app
@@ -62,12 +70,13 @@ function init() {
     inquirer.prompt(questions)
     .then(function(data) {
         console.log(data);
-    //var fileContent = generateMarkdown(data);
-   // writeToFile(fileName, data)
+    var userInput = generateMarkdown(data);
+    console.log(userInput)
+    writeToFile(userInput)
     });
 }
 
 // Function call to initialize app
 init();
 
-
+module.exports = questions;
